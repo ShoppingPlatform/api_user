b9e291dc78708c247f746ec812d271ea020d78ac
YohanNayanajith <yohannayanajith40@gmail.com>
2022-09-17 01:44:14 +0530

    set authentication

--- routes/user.js
+++ routes/user.js
@@ -9,28 +9,7 @@ const router = require("express").Router();
 const CryptoJS = require("crypto-js");
 
 //UPDATE
-// router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-//   if (req.body.password) {
-//     req.body.password = CryptoJS.AES.encrypt(
-//       req.body.password,
-//       process.env.PASS_SEC
-//     ).toString();
-//   }
-
-//   try {
-//     const updatedUser = await User.findByIdAndUpdate(
-//       req.params.id,
-//       {
-//         $set: req.body,
-//       },
-//       { new: true }
-//     );
-//     res.status(200).json(updatedUser);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-router.put("/:id", async (req, res) => {
+router.put("/:id",verifyTokenAndAuthorization, async (req, res) => {
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
       req.body.password,
@@ -53,15 +32,7 @@ router.put("/:id", async (req, res) => {
 });
 
 //DELETE
-// router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
-//   try {
-//     await User.findByIdAndDelete(req.params.id);
-//     res.status(200).json("User has been deleted...");
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-router.delete("/:id", async (req, res) => {
+router.delete("/:id",verifyTokenAndAuthorization, async (req, res) => {
   try {
     await User.findByIdAndDelete(req.params.id);
     res.status(200).json("User has been deleted...");
@@ -71,16 +42,7 @@ router.delete("/:id", async (req, res) => {
 });
 
 //GET USER
-// router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
-//   try {
-//     const user = await User.findById(req.params.id);
-//     const { password, ...others } = user._doc;
-//     res.status(200).json(others);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-router.get("/find/:id", async (req, res) => {
+router.get("/find/:id",verifyTokenAndAuthorization, async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     const { password, ...others } = user._doc;
@@ -91,7 +53,7 @@ router.get("/find/:id", async (req, res) => {
 });
 
 //GET ALL USER
-router.get("/", async (req, res) => {
+router.get("/",verifyTokenAndAuthorization, async (req, res) => {
   const query = req.query.new;
   try {
     const users = query
@@ -102,46 +64,9 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
-// router.get("/", verifyTokenAndAdmin, async (req, res) => {
-//   const query = req.query.new;
-//   try {
-//     const users = query
-//       ? await User.find().sort({ _id: -1 }).limit(5)
-//       : await User.find();
-//     res.status(200).json(users);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
-//GET USER STATS
 
-// router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
-//   const date = new Date();
-//   const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-
-//   try {
-//     const data = await User.aggregate([
-//       { $match: { createdAt: { $gte: lastYear } } },
-//       {
-//         $project: {
-//           month: { $month: "$createdAt" },
-//         },
-//       },
-//       {
-//         $group: {
-//           _id: "$month",
-//           total: { $sum: 1 },
-//         },
-//       },
-//     ]);
-//     res.status(200).json(data)
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-router.post("/email", async (req, res) => {
+router.post("/email",verifyTokenAndAuthorization, async (req, res) => {
   console.log(req.body.email);
   try {
     const user = await User.find({ email: req.body.email });
@@ -153,8 +78,8 @@ router.post("/email", async (req, res) => {
   }
 });
 
-
-router.get("/stats", async (req, res) => {
+//GET USER STATS
+router.get("/stats",verifyTokenAndAuthorization, async (req, res) => {
   const date = new Date();
   const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
 
